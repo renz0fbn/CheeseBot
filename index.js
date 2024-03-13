a69781a9f3a3f7b5b50bcec0fe46c7f183be53f2
renzofbn <renzofbn@tutanota.com>
2024-03-13 12:19:59 -0500

    Use a .env file and add standard code style

--- index.js
+++ index.js
@@ -1,88 +1,59 @@
-const {Client, Intents, Collection} = require('discord.js');
-require('dotenv').config();
+const { Client, Intents, Collection } = require('discord.js')
+// Load environment variables
+require('dotenv').config()
 
 // import global variables and data
-const config = require('./config.json');
-const i18n = require('i18n');
-const { join } = require('path');
-const { setInterval } = require('timers');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { join } = require('path')
+const { setInterval } = require('timers')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
+client.commands = new Collection()
+client.language = require('i18n')
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-client.commands = new Collection();
-client.language = require('i18n');
-
+// Set the language configuration
 client.language.configure({
-    locales: ['en', 'es'],
-    directory: join(__dirname, 'locales'),
-    defaultLocale: 'es',
-    retryInDefaultLocale: true,
-    objectNotation: true,
-    register: global,
+  locales: ['en', 'es'],
+  directory: join(__dirname, 'locales'),
+  defaultLocale: 'es',
+  retryInDefaultLocale: true,
+  objectNotation: true,
+  register: global,
 
-    logWarnFn: function (message) {
-        console.warn(message);
-    },
+  logWarnFn: function (message) {
+    console.warn(message)
+  },
 
-    logErrorFn: function (message) {
-        console.error(message);
-    },
+  logErrorFn: function (message) {
+    console.error(message)
+  },
 
-    missingKeyFn: function (locale, value) {
-        return value;
-    },
+  missingKeyFn: function (locale, value) {
+    return value
+  },
 
-    mustacheConfig: {
-        tags: ["{{","}}"],
-        disable: false
-    }
-    
+  mustacheConfig: {
+    tags: ['{{', '}}'],
+    disable: false
+  }
+
 })
 
-setInterval(updateStatus, 60000);
+// Every 60 seconds update the bot status
+setInterval(updateStatus, 60000)
 
-async function updateStatus(){
-    const guildNum = client.guilds.cache.size;
-    const memberNum = client.users.cache.size;
+async function updateStatus () {
+  const guildNum = client.guilds.cache.size
+  const memberNum = client.users.cache.size
 
-    await client.user.setActivity(`Servidores: ${guildNum} | Miembros: ${memberNum}`, { type: 'LISTENING' });
+  client.user.setActivity(`Servers: ${guildNum} and ${memberNum} members.`, { type: 'LISTENING' })
 }
 
-client.language.setLocale('es');
+// Set the default language to Spanish
+client.language.setLocale('es')
 
+// Load Events and Commands
+require('./handlers/events.js')(client)
+require('./handlers/commands.js')(client)
 
-require("./handlers/events.js")(client);
-require("./handlers/commands.js")(client);
-
-client.on('messageCreate', async (message) => {
-    if(message.content == "!!createEmb"){
-        const file = await message.attachments.first()?.url;
-        try {
-            message.channel.send('Reading the file! Fetching data...');
-        
-            // fetch the file from the external URL
-            const response = await fetch(file);
-        
-            // if there was an error send a message with the status
-            if (!response.ok)
-              return message.channel.send(
-                'There was an error with fetching the file:',
-                response.statusText,
-              );
-        
-            // take the response stream and read it to completion
-            const text = await response.text();
-        
-            if (text) {
-              message.channel.send(JSON.parse(text));
-            }
-          } catch (error) {
-            console.log(error);
-          }
-    }
-})
-
-client.login(config.token);
-// client.login(process.env.TOKEN);
+console.log('Logging in...')
+client.login(process.env.TOKEN)
--- mapCommands.js
+++ mapCommands.js
@@ -1,29 +1,27 @@
-const fs = require('fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v10');
-const { clientId, token } = require('./config.json');
+const fs = require('fs')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v10')
+require('dotenv').config()
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
 
-const rest = new REST({version: '10'}).setToken(token);
+createSlash()
 
-createSlash();
-
-async function createSlash(){
-    try{
-        const commands = [];
-        fs.readdirSync('./commands').forEach(async (category) => {
-            const comandFiles = fs.readdirSync(`./commands/${category}`).filter((archivo) => archivo.endsWith('.js'));
-            for (const file of comandFiles){
-                const command = require(`./commands/${category}/${file}`);
-                commands.push(command.data.toJSON());
-            }
-        })
-        await rest.put(
-            Routes.applicationCommands(clientId),
-            {body: commands}
-        )
-        console.log("Commands updated");
-    }
-    catch(e){
-        console.error(e);
-    }
+async function createSlash () {
+  try {
+    const commands = []
+    fs.readdirSync('./commands').forEach(async (category) => {
+      const comandFiles = fs.readdirSync(`./commands/${category}`).filter((archivo) => archivo.endsWith('.js'))
+      for (const file of comandFiles) {
+        const command = require(`./commands/${category}/${file}`)
+        commands.push(command.data.toJSON())
+      }
+    })
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    )
+    console.log(`Slash commands for bot: ${process.env.CLIENT_ID} have been registered`)
+  } catch (e) {
+    console.error(e)
+  }
 }
